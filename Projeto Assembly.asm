7928d401b93adcb605efa1961debb0a0f1841423
GuiMesc <36601466+GuiMesc@users.noreply.github.com>
2021-02-02 18:31:20 -0300

    Atualização

--- Projeto Assembly.asm
+++ Projeto Assembly.asm
@@ -1,9 +1,16 @@
 .data
 fout: .asciiz "palavras.txt"
-palavra: .asciiz
 linha: .asciiz "\n"
+palavra: .asciiz
 
 .text
+#GERAR NUMERO ALEATORIO ENTRA AS 10 PALAVRAS
+li $v0, 42
+addi $a0, $a0, 1
+addi $a1, $a1, 10
+syscall
+add $t1, $zero, $a0	#GUARDA VALOR ALEATORIO GERADO EM $T1
+
 #ABERTURA DE ARQUIVO
 li $v0, 13
 la $a0, fout
@@ -12,26 +19,32 @@ li $a2, 0
 syscall
 move $s6, $v0
 
-#LEITURA DE PALAVRA
-lb $t0, linha		#GUARDA VALOR DA QUEBRA DE LINHA
-addi $t1, $zero, 0	#DESLOCA NA MEMORIA
-loop:
-  li $v0, 14
-  move $a0, $s6
-  la $a1, palavra
-  beq $a2, $t0, sailoop
-  lb $a2, fout($t1)
-  addi $t1, $t1, 1
-  syscall
-  j loop
-sailoop:
-
+#LEITURA DA PALAVRA
+lb $t2, linha	#GUARDA O CARACTERE LINHA EM $T2
+la $a1, palavra
+j whilePalavra
+whileLinha:
+addi $t4, $t4, 1	#CONTADOR PARA RODAR A MESMA QUANTIDADE DE VEZES DO NUMERO ALEATORIO GERADO
+beq $t4, $t1, saiwhile
+la $a1, palavra		#CARREGA O ENDEREÇO NOVAMENTE PARA SOBRESCREVER A PALAVRA QUANDO FOR PARA OUTRA LINHA
+ whilePalavra:
+   li $v0, 14
+   move $a0, $s6
+   addi $a2, $zero, 1
+   syscall
+   lb $t3, 0($a1)		#LE CARACTERE A CARACTERE E GUARDA EM $T3, INCREMENTANDO O ENDEREÇO DA PALAVRA
+   beq $t3, $t2, whileLinha	#QUANDO ENCONTRA A QUEBRA DE LINHA, VAI PARA "WHILELINHA" E AVANÇA PARA A PROXIMA
+   addi $a1, $a1, 1
+   j whilePalavra
+saiwhile:
+  
 #FECHAMENTO DE ARQUIVO
 li $v0, 16
 move $a0, $s6
 syscall
 
-#TESTE PARA SABER SE A PALAVRA FOI REALMENTE LIDA
+#IMPRIMIR STRING LIDA PARA TESTE
 li $v0, 4
+la $a1, palavra
 move $a0, $a1
 syscall
